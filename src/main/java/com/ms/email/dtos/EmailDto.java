31bd46e0c2191a226a045c342ae1dbea629ec13f
dyegokleodon <dyego.kleodon@gmail.com>
2023-06-02 12:43:10 -0300

    Create EmailDto

---
+++ src/main/java/com/ms/email/dtos/EmailDto.java
@@ -0,0 +1,16 @@
+package com.ms.email.dtos;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+
+@Data
+public class EmailDto {
+
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+}
