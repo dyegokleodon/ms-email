55b491e3bfbc0e6d563fb9ae525d0487a83f4d54
dyegokleodon <dyego.kleodon@gmail.com>
2023-06-06 15:20:39 -0300

    Create logs configurations

--- src/main/java/com/ms/email/controllers/EmailController.java
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -5,6 +5,8 @@ import com.ms.email.models.EmailModel;
 import com.ms.email.services.EmailService;
 import jakarta.validation.Valid;
 import org.apache.coyote.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -22,6 +24,9 @@ import java.util.List;
 
 @RestController
 public class EmailController {
+
+    Logger logger = LogManager.getLogger(EmailController.class);
+
     @Autowired
     EmailService emailService;
 
@@ -36,6 +41,12 @@ public class EmailController {
 
     @GetMapping("/sending-email")
     public ResponseEntity<Page<EmailModel>> getAllEmails(@PageableDefault(page = 0, size = 10, sort="emailId", direction = Sort.Direction.ASC)Pageable pageable){
+        logger.trace("TRACE");
+        logger.debug("DEBUG");
+        logger.info("INFO");
+        logger.warn("WARN");
+        logger.error("ERROR");
+        logger.fatal("FATAL");
         return ResponseEntity.status(HttpStatus.OK).body(emailService.findAll(pageable));
     }
 }
